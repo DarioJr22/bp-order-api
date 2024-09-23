681377c727d7a71cccada34fe4995ba6d44faf08
dario.junior <dariojr1233@gmail.com>
2024-09-23 15:30:17 -0300

    Troca de email - (BravanParts)

--- src/modules/messages/email.controller.ts
+++ src/modules/messages/email.controller.ts
@@ -13,7 +13,7 @@ export class EmailController {
     const emailHtml = await this.emailService.generateOrderTemplate(order);
 
     const result = await this.emailService.sendMail(
-      'dariojr1233@gmail.com', 
+      'executivo@bravanparts.com', 
       'Detalhes do Pedido - Bravan Parts', 
       `O pedido tem o total de R$${order.price}`,
       emailHtml
